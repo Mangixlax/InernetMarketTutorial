1537c3e6aea86b199ff79a875820e0776f9491a3
Aslan Dzhangetov <>
2020-10-25 16:11:35 +0300

    fix merge fonflict

---
+++ src/components/v-cart-item.vue
@@ -0,0 +1,56 @@
+
+<template>
+	<div class="v-cart-item">
+        <img class="v-cart-item__image" :src="require('../assets/images/' + cart_item_data.image)" alt="product">
+        <div>
+            <p>{{cart_item_data.name}}</p>
+            <p>{{cart_item_data.price}}</p>
+            <p>{{cart_item_data.article}}</p>
+        </div>
+        <div class="v-cart-item__quantity">
+            <button>Delete</button>
+        </div>
+	</div>
+</template>
+<script>
+  export default {
+    name: 'v-cart-item',
+    components: {},
+    props: {
+      cart_item_data: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    data(){
+      return {
+
+      }
+    },
+    methods: {
+        
+    },
+    computed: {
+        
+    },
+
+    mounted() {
+      this.$set(this.cart_item_data, 'quantity', 1)
+    }
+    
+   
+  }
+</script>
+
+<style lang="scss">
+    .v-cart-item {
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+        align-items: center;
+        box-shadow: 0 0 8px 0 #e0e0e0;
+        &__image {
+            max-width: 50px;
+        }
+    }
+</style>
--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,20 +1,9 @@
 <template>
   <div class="v-catalog-item">
-<<<<<<< HEAD
-    <img :src="`../assets/images/${id}.jpg`" alt="product">
-    <p id class="v-catalog-item__name">{{product_data.name}}</p>
-    <p class="v-catalog-item__price">{{product_data.price}}</p>
-    <button >Add to cart</button>
-=======
     <img class="v-catalog-item__image" :src="require('../assets/images/' + product_data.image)" alt="product">
     <p class="v-catalog-item__name">{{product_data.name}}</p>
     <p class="v-catalog-item__price">{{Math.floor(product_data.price)}}</p>
-<<<<<<< HEAD
-    <button @click="sendDataToParent">Add to cart</button>
->>>>>>> c5edd9d44d45b3636edf2e132850bc6f5afa6ceb
-=======
     <button @click="addToCart">Add to cart</button>
->>>>>>> 063aece10d309d324dcb69ccc24e10150c564c22
   </div>
 </template>
 
